d98d08d567fb477a9224273fcc24fc1e57d0506d
Robert Brown <robrownn@gmail.com>
2021-06-03 00:52:29 -0400

    updated publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,5 +2,5 @@
 
 set -x
 
-dotnet publish ..\\src\\rudimentary -c Release
-cp -r ..\\src\\rudimentary\\bin\\Release\\net5.0\\wwwroot\\ ..\\dist
+dotnet publish ..\\src\\rudimentary -c Release -o ..\\artifacts
+cp -r ..\\src\\rudimentary\\bin\\Release\\net5.0\\wwwroot\\. ..\\docs
